445c1fb3c766e20a8bfa7560f25e4f7366bc1e8c
elfishhenry <elfishhenry23@gmail.com>
2024-06-24 16:14:49 +0200

    Copied and pasted a script that loads all cogs at the same time

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ intents.guilds = True
 intents.guild_messages = True
 
 
-bot = discord.Bot(intents=discord.Intents.all())
+bot = discord.Bot(command_prefix="!", intents=discord.Intents.all())
 
 client = bot
 
@@ -29,14 +29,32 @@ async def on_ready():
 
 
 
-
+keep_alive()
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+#credit to this repo"https://github.com/micfun123/Simplex_bot" for the snippit of code below.
 
-bot.load_extension("cogs.Music")
-print("cogs proabbyl loaded")
+def start_bot(client):
+    lst = [f for f in listdir("c:/Users/User/Documents/lets try making a bot again/hm/cogs/") if isfile(join("cogs/", f))]
+    no_py = [s.replace(".py", "") for s in lst]
+    startup_extensions = ["cogs." + no_py for no_py in no_py]
+    try:
+        for cogs in startup_extensions:
+            client.load_extension(cogs)  # Startup all cogs
+            print(f"Loaded {cogs}")
+
+        print("\nAll Cogs Loaded\n===============\nLogging into Discord...")
+        client.run(
+            TOKEN
+        )  # Token do not change it here. Change it in the .env if you do not have a .env make a file and put DISCORD_TOKEN=Token
+
+    except Exception as e:
+        print(
+            f"\n###################\nPOSSIBLE FATAL ERROR:\n{e}\nTHIS MEANS THE BOT HAS NOT STARTED CORRECTLY!"
+        )
 
 
-bot.run(TOKEN)
+start_bot(client)
+
